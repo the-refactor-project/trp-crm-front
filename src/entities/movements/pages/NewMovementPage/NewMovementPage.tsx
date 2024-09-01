dd7dd16e591431a47b183cc07a69ce75182979cd
Mario González <mario@mariogl.com>
2024-09-01 11:49:29 +0200

    Route new movement page

---
+++ src/entities/movements/pages/NewMovementPage/NewMovementPage.tsx
@@ -0,0 +1,9 @@
+const NewMovementPage: React.FC = () => {
+  return (
+    <>
+      <h1>Nuevo movimiento</h1>
+    </>
+  );
+};
+
+export default NewMovementPage;
---
+++ src/router/paths.ts
@@ -0,0 +1,20 @@
+type BasePath = "movements";
+type Paths = Record<BasePath, Record<"root", string> & Record<string, string>>;
+
+const paths: Paths = {
+  movements: {
+    root: "movimientos",
+    new: "crear",
+  },
+};
+
+export const getPath = (
+  base: BasePath,
+  child?: keyof (typeof paths)[BasePath],
+): string => {
+  if (child) {
+    return `${paths[base].root}/${paths[base][child]}`;
+  }
+
+  return paths[base].root;
+};
