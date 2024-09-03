e43f528422f34b382bd1eb79845211ce80af8b35
Mario González <mario@mariogl.com>
2024-09-03 15:01:14 +0200

    Update movement

--- src/entities/movements/mutations/movementsMutations.ts
+++ src/entities/movements/mutations/movementsMutations.ts
@@ -28,6 +28,33 @@ export const useAddMovementMutation = () => {
   });
 };
 
+export const useUpdateMovementMutation = () => {
+  return useMutation({
+    mutationFn: async (movement: MovementStructure) => {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/movements`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(movement),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error("Error al modificar el movimiento");
+      }
+
+      const { movement: updatedMovement } = (await response.json()) as {
+        movement: MovementStructure;
+      };
+
+      return updatedMovement;
+    },
+  });
+};
+
 export const useDeleteMovement = () => {
   return useMutation({
     mutationFn: async (movementId: MovementStructure["_id"]) => {
--- src/entities/movements/pages/EditMovementPage/EditMovementPage.tsx
+++ src/entities/movements/pages/EditMovementPage/EditMovementPage.tsx
@@ -1,24 +1,25 @@
 import { useNavigate, useParams } from "react-router-dom";
 import MovementForm from "@/components/MovementForm";
-import { MovementFormDataStructure } from "@movements/schema";
-import { useAddMovementMutation } from "@movements/mutations/movementsMutations";
-import useAppStore from "@/store/useAppStore";
+import {
+  MovementFormDataStructure,
+  MovementStructure,
+} from "@movements/schema";
+import { useUpdateMovementMutation } from "@movements/mutations/movementsMutations";
 import Loading from "@/components/Loading";
 import { getPath } from "@/router/paths";
 import { useMovementQuery } from "@movements/queries/useMovementQuery";
 import Error from "@/components/Error";
 
 const EditMovementPage: React.FC = () => {
-  const { addMovement } = useAppStore((state) => state);
   const { movementId } = useParams<{ movementId: string }>();
   const { data, isLoading, isError } = useMovementQuery(movementId!);
-  const { mutateAsync, isPending } = useAddMovementMutation();
+  const { mutateAsync, isPending } = useUpdateMovementMutation();
   const navigate = useNavigate();
 
-  const createMovement = async (newMovementData: MovementFormDataStructure) => {
-    const newMovement = await mutateAsync(newMovementData);
+  const updateMovement = async (movement: MovementStructure) => {
+    await mutateAsync(movement);
 
-    addMovement(newMovement);
+    // addMovement(updateMovement);
 
     navigate(getPath("movements"));
   };
@@ -27,7 +28,9 @@ const EditMovementPage: React.FC = () => {
     <>
       <h1>Editar movimiento</h1>
       <MovementForm
-        onSubmit={createMovement}
+        onSubmit={(formValues) =>
+          updateMovement(formValues as MovementStructure)
+        }
         initialValues={data as MovementFormDataStructure}
       />
       {isError && <Error message="No se han podido cargar los datos" />}
